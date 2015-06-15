e8f0798a02461b37ac66fab21bb03a578bd8b0c1
Ziya Suzen <ziya.suzen@bjss.com>
2015-06-15 14:17:45 +0100

    Example chef recipe

---
+++ cookbooks/learn_chef_iis/metadata.rb
@@ -0,0 +1,7 @@
+name 'learn_chef_iis'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures learn_chef_iis'
+long_description 'Installs/Configures learn_chef_iis'
+version '0.1.0'
---
+++ cookbooks/learn_chef_iis/recipes/default.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: learn_chef_iis
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+powershell_script 'Install IIS' do
+  code 'Add-WindowsFeature Web-Server'
+  guard_interpreter :powershell_script
+  not_if "(Get-WindowsFeature -Name Web-Server).Installed"
+end
+
+service 'w3svc' do
+  action [:enable, :start]
+end
+
+template 'c:\inetpub\wwwroot\Default.htm' do
+  source 'index.html.erb'
+end
